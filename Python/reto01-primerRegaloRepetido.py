3983ead3b9325cfde862156461f92e1127877b9f
Lara IV <ruben.toro1@gmail.com>
2025-10-02 16:33:02 -0500

    first python challenge completed

--- Python/reto01-primerRegaloRepetido.py
+++ Python/reto01-primerRegaloRepetido.py
@@ -1,7 +1,12 @@
-gifts1 = [3, 1, 2, 3, 4, 2, 5]
-arreglo = arreglo2 = 3
-def prepare_gifts(gifts):
-    print(arreglo, arreglo2)
-    return []
+# Santa Claus 🎅 ha recibido una lista de números mágicos que representan regalos 🎁, 
+# pero algunos de ellos están duplicados y deben ser eliminados para evitar confusiones. 
+# Además, los regalos deben ser ordenados en orden ascendente antes de entregárselos a los elfos.
+# Tu tarea es escribir una función que reciba una lista de números enteros (que pueden
+#  incluir duplicados) y devuelva una nueva lista sin duplicados, ordenada en orden ascendente.
+gifts1 = [6, 3, 1, 2, 3, 4, 2, 5]
 
-prepare_gifts(gifts1)
+def prepare_gifts(gifts):   
+    sorted = list(set(gifts))   
+    return sorted
+
+print(prepare_gifts(gifts1))
