c2c253136685baa7dad1d573184b12c835b8952c
Lara IV <ruben.toro1@gmail.com>
2025-10-02 08:46:40 -0500

    Archivos organizados

--- Python/reto01-primerRegaloRepetido.py
+++ Python/reto01-primerRegaloRepetido.py
@@ -1,7 +1,7 @@
-gifts1 = [3, 1, 2, 3, 4, 2, 5],
-
+gifts1 = [3, 1, 2, 3, 4, 2, 5]
+arreglo = arreglo2 = 3
 def prepare_gifts(gifts):
-    
+    print(arreglo, arreglo2)
     return []
 
 prepare_gifts(gifts1)
